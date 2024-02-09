b4120d31b4c5cf4deadfb105c97082b4d87c1d44
jho7260 <157347729+jho7260@users.noreply.github.com>
2024-02-09 15:48:36 +0900

    Delete submit.php

--- submit.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $userText = $_POST['userText'];
-  $userChoice = $_POST['userChoice'];
-
-  echo "typed texts: " . $userText . "<br>";
-  echo "radio options: " . $userChoice;
-}
-?>
